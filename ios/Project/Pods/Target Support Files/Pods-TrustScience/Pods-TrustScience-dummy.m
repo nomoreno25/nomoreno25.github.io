fb4fc990aa6f6a2e3453e0acd91ae6cc63785c95
Hailv17 <37890486+Hailv17@users.noreply.github.com>
2019-06-15 13:13:33 +0700

    #7
    xoa code web cu

--- ios/Project/Pods/Target Support Files/Pods-TrustScience/Pods-TrustScience-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TrustScience : NSObject
-@end
-@implementation PodsDummy_Pods_TrustScience
-@end
--- ios/Project/Pods/Target Support Files/Pods-TrustScienceCore/Pods-TrustScienceCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TrustScienceCore : NSObject
-@end
-@implementation PodsDummy_Pods_TrustScienceCore
-@end
